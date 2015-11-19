9bd8d9ff6801b186e6632819ea0270ee8e3736de
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-11-19 19:30:52 +0100

    Create Participant + Team API

---
+++ api/controllers/ParticipantController.js
@@ -0,0 +1,11 @@
+/**
+ * ParticipantController
+ *
+ * @description :: Server-side logic for managing participants
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
