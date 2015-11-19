1926ff0130f0b01357f3fc190c1d5a5713bd451a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-11-19 22:59:04 +0100

    GithubService to get number of commits for specific repository

---
+++ api/services/GithubService.js
@@ -0,0 +1,38 @@
+var GitHubApi = require("github");
+
+var github = new GitHubApi({
+    // required
+    version: "3.0.0",
+    // optional
+    debug: true,
+    protocol: "https",
+    host: "api.github.com", // should be api.github.com for GitHub
+    timeout: 5000,
+    headers: {
+        "user-agent": "Hackathon UTC Live wall" // GitHub is happy with a unique user agent
+    }
+});
+
+function stat(user, repo, cb){
+	github.repos.getStatsCommitActivity({
+		user: user,
+		repo: repo
+	}, cb);
+}
+
+module.exports = {
+	
+	// return number of commits of a given repo
+	getNbOfCommits: function(user, repo, cb){
+		stat(user, repo, function(err, res){
+            if(err) return cb(err);
+            
+            var total = 0;
+            for(var i = 0; i < res.length; i++){
+                total += res[i].total;
+            }
+            
+            cb(null, total);
+        });
+	}	
+};
