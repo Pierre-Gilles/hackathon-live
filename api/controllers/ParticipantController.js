7b6a36186e88dfa733eaad129463439057018043
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-11-24 22:48:21 +0100

    Team & participants controllers

--- api/controllers/ParticipantController.js
+++ api/controllers/ParticipantController.js
@@ -7,5 +7,13 @@
 
 module.exports = {
 	
+	create: function(req, res){
+		Participant.create({name: req.param('name'), team: req.param('team')}, function(err, participant){
+			if(err) return res.badRequest(err);
+			
+			return res.status(201).json(participant);
+		});
+	}
+	
 };
 
--- api/controllers/TeamController.js
+++ api/controllers/TeamController.js
@@ -7,5 +7,13 @@
 
 module.exports = {
 	
+	create: function(req, res) {
+		Team.create({name: req.param('name'), repository: req.param('repository') }, function(err, repo){
+			if(err) return res.badRequest(err);
+			
+			return res.status(201).json(repo);
+		});
+	}
+	
 };
 
