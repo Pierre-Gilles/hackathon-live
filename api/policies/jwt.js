83fe48da05a5180f953a36c8c4c9233d01a820e0
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-11-24 23:11:46 +0100

    added jsonwebtoken authentification

---
+++ api/policies/jwt.js
@@ -0,0 +1,27 @@
+/**
+ * jwt
+ *
+ * @module      :: Policy
+ * @description :: Simple policy to allow any valid JWT
+ * @docs        :: http://sailsjs.org/#!documentation/policies
+ *
+ */
+
+var jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next) {
+  
+  var token = req.headers['Authorization'];
+
+  // verify a token symmetric
+  jwt.verify(token, sails.config.jwt.secret, function(err, decoded) {
+      if(err) return res.forbidden();
+      
+      Admin.findOne({id: decoded.id}, function(err, user){
+          if(err || !user) return res.forbidden();
+
+          req.User = user;
+          return next();
+      });
+  });
+};
---
+++ config/jwt.js
@@ -0,0 +1,8 @@
+
+
+/**
+ * Json web token secret
+ * */
+module.exports.jwt = {
+	secret: 'dev'	
+};
