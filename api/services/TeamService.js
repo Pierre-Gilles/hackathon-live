7e7434eb3481653977a3adf946cdac47ba9557a1
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-11-25 17:43:00 +0100

    add points to old score

--- api/services/TeamService.js
+++ api/services/TeamService.js
@@ -1,13 +1,24 @@
 
 
-
+var sqlGetOneScores = 'SELECT MAX(points) as points, team.id, team.name, team.repository '
+				+ 'FROM score '
+				+ 'JOIN team ON (score.team = team.id) '
+				+ 'WHERE team.id = ? '
+				+ 'GROUP BY team.id '
+				+ 'ORDER BY points DESC; ';
 
 module.exports = {
 	updateTeamScore: function(repo, points , cb){
 		Team.find({repository: repo}, function(err, team){
 			if(err) return cb(err);
 			
-			Score.create({team: team.id, points: points}, cb);
+			Score.query(sqlGetOneScores, [team.id], function(err, scores){
+				if(err) return cb(err);
+				
+				points = points + parseInt(scores.points)/2;
+				
+				Score.create({team: team.id, points: points}, cb);
+			});
 		});
 	}	
 };
