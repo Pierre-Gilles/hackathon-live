861dcbf5c4283dd56a18e68fd150ff09deaa4346
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2015-11-25 12:52:21 +0100

    Load app module before controller

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -28,6 +28,8 @@ var jsFilesToInject = [
 
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
+  
+  'js/app/app.module.js',
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
